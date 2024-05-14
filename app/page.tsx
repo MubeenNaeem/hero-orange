7d70bcebb455fbe7cf611f6c3e0d98e4f56b4e74
MubeenNaeem <mubeen.naeem138@gmail.com>
2024-05-14 10:12:33 +0500

    function issue fixed

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={`${mainFont.className}`}>
       {/* Background */}
-      <div className="bg-prime-50 absolute -z-50 h-screen w-full bg-opacity-70"></div>
+      <div className="absolute -z-50 h-screen w-full bg-prime-50 bg-opacity-70"></div>
 
       {/* Main Div */}
       <div className="mx-auto flex max-w-3xl">
@@ -36,7 +36,7 @@ export default function Home() {
                 Mubeen Naeem{" "}
               </h1>
               <p
-                className={`text-red mt-4 text-xl italic ${serifFont.className}`}
+                className={`mt-4 text-xl italic text-red ${serifFont.className}`}
               >
                 <span>UI designer</span>
                 <span className="text-black"> & </span>
@@ -62,10 +62,10 @@ export default function Home() {
 
 const iconSize = 16;
 
-export function ProfileIcons() {
+function ProfileIcons() {
   return (
-    <div className="bg-dark flex h-10 items-center rounded-full border border-gray-200  p-1.5">
-      <div className="bg-prime-50 flex items-center rounded-full">
+    <div className="flex h-10 items-center rounded-full border border-gray-200 bg-dark  p-1.5">
+      <div className="flex items-center rounded-full bg-prime-50">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 512 512"
@@ -75,7 +75,7 @@ export function ProfileIcons() {
         >
           <path d="M256 8C119.3 8 8 119.3 8 256s111.3 248 248 248 248-111.3 248-248S392.7 8 256 8zm164 114.4c29.5 36 47.4 82 47.8 132-7-1.5-77-15.7-147.5-6.8-5.8-14-11.2-26.4-18.6-41.6 78.3-32 113.8-77.5 118.3-83.5zM396.4 97.9c-3.8 5.4-35.7 48.3-111 76.5-34.7-63.8-73.2-116.2-79-124 67.2-16.2 138 1.3 190.1 47.5zm-230.5-33.3c5.6 7.7 43.4 60.1 78.5 122.5-99.1 26.3-186.4 25.9-195.8 25.8C62.4 147.2 106.7 92.6 165.9 64.6zM44.2 256.3c0-2.2 0-4.3 .1-6.5 9.3 .2 111.9 1.5 217.7-30.1 6.1 11.9 11.9 23.9 17.2 35.9-76.6 21.6-146.2 83.5-180.5 142.3C64.8 360.4 44.2 310.7 44.2 256.3zm81.8 167.1c22.1-45.2 82.2-103.6 167.6-132.8 29.7 77.3 42 142.1 45.2 160.6-68.1 29-150 21.1-212.8-27.9zm248.4 8.5c-2.2-12.9-13.4-74.9-41.2-151 66.4-10.6 124.7 6.8 131.9 9.1-9.4 58.9-43.3 109.8-90.8 142z" />
         </svg>
-        <div className="bg-dark h-7 w-[1px]" />
+        <div className="h-7 w-[1px] bg-dark" />
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 576 512"
@@ -85,7 +85,7 @@ export function ProfileIcons() {
         >
           <path d="M232 237.2c31.8-15.2 48.4-38.2 48.4-74 0-70.6-52.6-87.8-113.3-87.8H0v354.4h171.8c64.4 0 124.9-30.9 124.9-102.9 0-44.5-21.1-77.4-64.7-89.7zM77.9 135.9H151c28.1 0 53.4 7.9 53.4 40.5 0 30.1-19.7 42.2-47.5 42.2h-79v-82.7zm83.3 233.7H77.9V272h84.9c34.3 0 56 14.3 56 50.6 0 35.8-25.9 47-57.6 47zm358.5-240.7H376V94h143.7v34.9zM576 305.2c0-75.9-44.4-139.2-124.9-139.2-78.2 0-131.3 58.8-131.3 135.8 0 79.9 50.3 134.7 131.3 134.7 61.3 0 101-27.6 120.1-86.3H509c-6.7 21.9-34.3 33.5-55.7 33.5-41.3 0-63-24.2-63-65.3h185.1c.3-4.2 .6-8.7 .6-13.2zM390.4 274c2.3-33.7 24.7-54.8 58.5-54.8 35.4 0 53.2 20.8 56.2 54.8H390.4z" />
         </svg>{" "}
-        <div className="bg-dark h-7 w-[1px]" />
+        <div className="h-7 w-[1px] bg-dark" />
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 448 512"
@@ -100,8 +100,8 @@ export function ProfileIcons() {
   );
 }
 
-export function OrangeCircle() {
+function OrangeCircle() {
   return (
-    <div className="from-start to-end absolute bottom-0 left-0 right-0 top-0 -z-10 m-auto h-80 w-80 rounded-full bg-gradient-to-b blur-lg" />
+    <div className="absolute bottom-0 left-0 right-0 top-0 -z-10 m-auto h-80 w-80 rounded-full bg-gradient-to-b from-start to-end blur-lg" />
   );
 }
